de9ec543ca42d655c40d8ee71a094d2e1bc50598
jjjjose <jjjjoseignacio@gmail.com>
2020-07-17 14:36:13 -0400

    mejora, pasando direco a sapper

--- src/pages/raul/index.svelte
+++
@@ -1,1 +0,0 @@
-<div>es parte de raul</div>
---
+++ src/routes/raul/index.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`raul.json`)
+      .then(r => r.json())
+      .then(posts => {
+        return { posts };
+      });
+  }
+</script>
+
+<script>
+  export let posts;
+</script>
+
+<svelte:head>
+  <title>Raul</title>
+</svelte:head>
+<div>
+hola amigos
+</div>
